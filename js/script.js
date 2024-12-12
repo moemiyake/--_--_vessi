83226449d1a1c2deafb47ba01ace582fe9e43b4b
moemiyake <shootea1920@gmail.com>
2024-12-12 11:19:31 +0900

    音の追加

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-new Audio()
+//オーディオを登録する
+const audio = new Audio("../music/01 Overture.mp3");
+
+
+// オーディオを連れてくる
+// const audio = document.querySelector("audio");
+
+
+//onボタンを連れてくる
+const on = document.querySelector("#on");
+//offボタンを連れてくる
+const off = document.querySelector("#off");
+
+
+//ONボタンを押したら音が流れる
+on.addEventListener("click", () => {
+    audio.play();
+});
+//OFFボタンを押したら音が止まる
+off.addEventListener("click", () => {
+    audio.pause();
+});
