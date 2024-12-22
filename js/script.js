cfed301fa1b4bb38c9e8af8b36128eb2668a99d9
moe miyake <shootea1920@gmail.com>
2024-12-22 17:11:21 +0900

    12/22 スタイル - Swiperの追加

--- js/script.js
+++ js/script.js
@@ -16,3 +16,82 @@ on.addEventListener("click", () => {
 off.addEventListener("click", () => {
     audio.pause();
 });
+
+
+
+// ★slide2を一定速度に走らせたい
+// Swiper
+
+// slide1
+// ・自動再生させる
+const slide1 = new Swiper('#slide1', {
+    loop: true,
+    speed: 8000,
+    autoplay:  {
+        delay: 0,
+    },
+    
+});
+window.addEventListener('load', function(){
+    initSwiper(); // ページ読み込み後に初期化
+  });
+
+// ・ドラックしたときの調整
+// const slide1y = new Swiper('#slide1', {
+//   freeMode: {
+//     enabled: true,
+//     momentum: false,
+//   },
+  
+//   on: {
+//     touchEnd: (swiper) => {
+//       swiper.slideTo(swiper.activeIndex + 1);
+//     }
+//   }
+// });
+
+
+// slide2
+// ・自動再生させる(逆再生)
+const slide2 = new Swiper('#slide2', {
+    loop: true,
+    speed: 8000,
+    autoplay:  {
+        delay: 0,
+        reverseDirection: true,
+    },
+    
+});
+window.addEventListener('load', function(){
+    initSwiper(); // ページ読み込み後に初期化
+  });
+
+// ・ドラックしたときの調整
+const slide2y = new Swiper('#slide2', {
+  freeMode: {
+    enabled: true,
+    momentum: false,
+  },
+  
+  on: {
+    touchEnd: (swiper) => {
+      swiper.slideTo(swiper.activeIndex + 1);
+    }
+  }
+});
+
+
+// slide3
+// ・自動再生させる(逆再生)
+const slide3 = new Swiper('#slide3', {
+    reverseDirection: true, //★逆できない？
+    loop: true,
+    speed: 8000,
+    autoplay:  {
+        delay: 0,
+    },
+    
+});
+window.addEventListener('load', function(){
+    initSwiper(); // ページ読み込み後に初期化
+  });
